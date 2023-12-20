5ded7195f44e6fb692a401517516fd0826decb3e
Yuriy Ostashevskyi <80325154+Ostashevskyi@users.noreply.github.com>
2023-12-20 15:42:31 +0200

    added orders

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,12 @@ import { searchIcon, userIcon, cartIcon } from "@/utils/icon_imports";
 const Header = () => {
   const data = useLogo();
 
+  const links = [
+    { id: 0, title: "Shop", link: "/catalog" },
+    { id: 1, title: "Blog", link: "/blog" },
+    { id: 2, title: "Our Story", link: "/about" },
+  ];
+
   return (
     <header
       className="mt-12 pb-4 border-b-2 border-light_gray flex justify-between items-center
@@ -22,15 +28,13 @@ const Header = () => {
       <div className="flex">
         <div className="pr-12 border-r-2 border-light_gray">
           <ul className="flex gap-16 text-base">
-            <li>
-              <HeaderLink to="/catalog">Shop</HeaderLink>
-            </li>
-            <li>
-              <HeaderLink to="/blog">Blog</HeaderLink>
-            </li>
-            <li>
-              <HeaderLink to="/about">Our Story</HeaderLink>
-            </li>
+            {links.map((link) => {
+              return (
+                <li key={link.id} className="heading5D font-medium">
+                  <NavLink to={link.link}>{link.title}</NavLink>
+                </li>
+              );
+            })}
           </ul>
         </div>
         <div className="ml-12">
--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -1,9 +1,19 @@
 import React, { useState, useMemo } from "react";
 import Wrapper from "@/components/Wrapper";
 import Dashboard from "@/pages/account/Dashboard";
+import Orders from "@/pages/account/Orders";
 
 const Account = () => {
   const [activeElement, setActiveElement] = useState("Dashboard");
+
+  const buttons = [
+    { id: 0, title: "Dashboard" },
+    { id: 1, title: "Orders" },
+    { id: 2, title: "Downloads" },
+    { id: 3, title: "Addresses" },
+    { id: 4, title: "Account Details" },
+    { id: 5, title: "Logout" },
+  ];
 
   const handleClick = (e) => {
     setActiveElement(e.target.innerHTML);
@@ -13,6 +23,8 @@ const Account = () => {
     switch (activeElement) {
       case "Dashboard":
         return <Dashboard />;
+      case "Orders":
+        return <Orders />;
       default:
         return;
     }
@@ -23,25 +35,20 @@ const Account = () => {
       <main className="mt-24 mb-52">
         <h1 className="text-center mb-16">My Account</h1>
         <div>
-          <ul className="flex gap-12 border-b pb-8 border-light_gray mb-10">
-            <li>
-              <button onClick={(e) => handleClick(e)}>Dashboard</button>
-            </li>
-            <li>
-              <button onClick={(e) => handleClick(e)}>Orders</button>
-            </li>
-            <li>
-              <button onClick={(e) => handleClick(e)}>Downloads</button>
-            </li>
-            <li>
-              <button onClick={(e) => handleClick(e)}>Addresses</button>
-            </li>
-            <li>
-              <button onClick={(e) => handleClick(e)}>Account Details</button>
-            </li>
-            <li>
-              <button onClick={(e) => handleClick(e)}>Logout</button>
-            </li>
+          <ul className="flex gap-12 border-b border-light_gray mb-10">
+            {buttons.map((btn) => {
+              return (
+                <li
+                  key={btn.id}
+                  className={`${
+                    activeElement === btn.title &&
+                    "border-b border-black pb-8 translate-y-px"
+                  }`}
+                >
+                  <button onClick={(e) => handleClick(e)}>{btn.title}</button>
+                </li>
+              );
+            })}
           </ul>
         </div>
         <div>{displayElems}</div>
