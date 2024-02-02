49a0df82d438a2f1b1accfad7846ce0ab4f48b17
Yuriy Ostashevskyi <80325154+Ostashevskyi@users.noreply.github.com>
2024-02-02 13:24:59 +0200

    change logout button

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import { ButtonXL } from "@/components/Buttons/ButtonXL";
 
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Logout = () => {
   const { logout } = useAuth0();
   return (
-    <div className="mb-[10%] xs:mb-40 sm:mb-40 md:mb-[20%]">
-      <button
+    <div className="mb-[10%] xs:mb-40 sm:mb-40 md:mb-[20%] max-w-xs">
+      <ButtonXL
         onClick={() =>
           logout({ logoutParams: { returnTo: window.location.origin } })
         }
         className="mx-4 text-text"
       >
         Logout
-      </button>
+      </ButtonXL>
     </div>
   );
 };
